d4efbfdb9a8f4ebfe65f1d273fa2f92fa67fc4d0
bogdantoda <bogdan.toda@yahoo.com>
2017-12-10 02:32:45 +0200

    Add NodeJS server to service as API to the react app
    Affects JIRA-3

---
+++ server.js
@@ -0,0 +1,30 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+const express = require('express');
+const app = express()
+
+var client = new Twitter(config.twitter);
+
+console.log(client);
+
+app.get('/', (req, res) => res.sendFile('./public/index.html', { "root": __dirname }))
+
+app.get('/tweets/:name/:max_id/:count', (req, res) => {
+    var name = req.params.name;
+    var max_id = req.params.max_id;
+    var params = {
+        q: name,
+        lang: "en",
+        count: req.params.count ? req.params.count : 10
+    };
+    if (max_id && max_id != "0") {
+        params.max_id = max_id;
+    }
+    console.log(params);
+    client.get("search/tweets", params, (error, tweets, response) => {
+        res.json(tweets);
+    })
+})
+
+
+app.listen(3001, () => console.log('Example app listening on port 3001!'))
